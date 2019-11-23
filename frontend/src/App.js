ca7c05a223d4430915bf0619efadbf3d28898ebf
krioux5 <krioux5@uwo.ca>
2019-11-23 15:15:37 -0500

    formatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,75 +18,17 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-
- 
- 
-      <Router>
-      <Switch>
-
+        <Router>
+          <Switch>
             <Route path='/' exact>
               <Signup />
             </Route>
             <Route path="/accountcreation" exact >
-              <AccountCreation />
+                <AccountCreation />
             </Route>
-
           </Switch>
         </Router>
-
-    </header>
-
-
+      </header>
     </div>
-
   );
 }
-
-// function Home() {
-//    return(
-//      <p>hey</p>
-//    );  
-//   <div className="App">
-//     <header className="App-header">
-
-//     <img src={logo} className="App-logo" alt="logo" />
-
-//     <Typography variant="h5" component="h5">
-//       Welcome to Portfol.io
-//     </Typography>
-
-//     <br></br>
-
-//     <Button variant="contained" color="primary" onClick={handleLogin}>
-//       sign in with github
-//     </Button>
-
-//     <br></br>
-
-//     <Link to="/accountcreation">
-//      <Button variant="contained" color="primary" >
-//         REDIRECT TO ACCOUNT CREATION FORM
-//      </Button>
-//     </Link>
-
-//   </header>
-// </div>
-
-//}
-
-// function About() {
-//   return(
-//     <div className="about">
-//     <header className="App-header">
-//     <p>Account Creation</p>
-
-//     </header>
-    
-//     </div>
-//   ) 
-// }
-
-// function Users() {
-//   return <h2>Users</h2>;
-// }
-
--- frontend/src/components/Signup/Signup.js
+++ frontend/src/components/Signup/Signup.js
@@ -43,7 +43,6 @@ return(
 
 <br></br>
 
-
 </header>
 
 
