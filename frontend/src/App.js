6c9ac0fee4deb968cdded360441b3b928543383d
krioux5 <krioux5@uwo.ca>
2019-11-23 11:44:50 -0500

    button onaction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import 'typeface-roboto';
+
+function handleClick(e) {
+  e.preventDefault();
+  console.log('The link was clicked.');
+}
 
 function App() {
   return (
@@ -9,19 +15,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Portfol.io
         </p>
-        <Button variant="contained" color="primary">
-          BUTTON FOR ALI TO CLICK
+        <Button variant="contained" color="primary" onClick={handleClick}>
+          sign in with github
         </Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
       </header>
     </div>
   );
