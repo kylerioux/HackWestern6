bece3f29b6b68109e719bf840c2197f47b52dfc6
Ali Tavakoli <atavako5@uwo.ca>
2019-11-23 20:18:42 -0500

    sss

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ var cors = require('cors');
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
+const passport = require("passport");
 var mongoose = require("mongoose");
 
 //registering models
